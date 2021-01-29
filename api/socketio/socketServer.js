fda886316c1c27a79ebd75fecb073033b4f48e3b
Shirly Madushanka <shirlyekanayake@gmail.com>
2021-01-29 20:57:51 +0530

    socketio server file missing bug fixed!

---
+++ api/socketio/socketServer.js
@@ -0,0 +1,63 @@
+const socketIO = require('socket.io');
+const jwt = require('jsonwebtoken');
+const UserBase = require('../models/UserBase');
+
+var io = null;
+
+module.exports = {
+
+    connect: function (server) {
+        console.log("starting socket.io server...");
+        try {
+            io = socketIO(server);
+            io.use(async (socket, next) => {
+                try {
+                    const token = socket.handshake.auth.token;
+                    const decodedPayload = jwt.verify(token, process.env.SECRET);
+                    socket.user = decodedPayload;
+                    next();
+                } catch (error) {
+                    next(new Error(error.message));
+                }
+
+            });
+            io.sockets.on('connection', async (socket) => {
+                console.log("new connection from - "+socket.user.user_id);
+                let user = socket.user;
+                const userDets = await UserBase.findById(user.user_id);
+
+                if (user.role === "owner") {
+                    socket.join("property:" + userDets.properties[0].toString());
+                } else if (user.role === "operator") {
+                    socket.join("property:" + userDets.work_on.toString());
+                }
+                socket.join(userDets._id.toString());
+
+                socket.emit('connection_success', "successfully connected!");
+
+                socket.on('disconnet', () => {
+                    console.log("Client disconnected!");
+                });
+            });
+        } catch (error) {
+            console.log(error);
+        }
+
+    },
+
+    emit: function (event, values) {
+        if (io) {
+            io.sockets.emit(event, values);
+        } else {
+            console.log("IO not defined!");
+        }
+    },
+
+    emitToRoom: function (room, event, values) {
+        if (io) {
+            io.to(room.toString()).emit(event, values);
+        } else {
+            console.log("IO not defined!");
+        }
+    }
+}
